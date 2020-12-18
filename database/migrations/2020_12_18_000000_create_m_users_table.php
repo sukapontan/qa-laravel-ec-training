b84af8b0b0c09713f538eb85f010b2856803641e
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-18 18:51:54 +0900

    add:m_sales_statusesテーブルのマイグレーション作成

--- database/migrations/2020_12_18_000000_create_m_users_table.php
+++ database/migrations/2020_12_18_000000_create_m_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -41,6 +41,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('m_users');
     }
 }
