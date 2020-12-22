94cdd858511ef89c7be5850a15a8a30fb2424c6b
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-22 16:39:54 +0900

    add:topページの編集

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div class="center jumbotron bg-warning">
-
-        <div class="text-center text-white">
-            <h1>laravel-共同開発</h1>
-        </div>
-
-    </div>
-
+    {{-- <div class="center jumbotron bg-warning"></div> --}}
 @endsection
