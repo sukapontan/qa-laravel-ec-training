b9430934b99b86717137ec8c869357b80e1208cf
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-22 14:24:10 +0900

    add:header.blade.phpを追加

---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,12 @@
+<header class="navbar navbar-dark bg-warning">
+
+    <a class="navbar-brand text-dark" style="font-size:x-large;" href="">
+        探求学園Laravel専攻
+    </a>
+
+    <ul class="list-inline navbar-brand text-dark">
+        <li class="list-inline-item"><a class="nav-link" href="#">ログイン</a></li>
+        <li class="list-inline-item"><a class="nav-link" href="#">新規登録</a></li>
+    </ul>
+
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,19 +18,6 @@
 
     <body>
 
-    <header class="navbar navbar-dark bg-warning">
-
-            <a class="navbar-brand text-dark" style="font-size:x-large;" href="">
-                探求学園Laravel専攻
-            </a>
-
-            <ul class="list-inline navbar-brand text-dark">
-                <li class="list-inline-item"><a class="nav-link" href="#">ログイン</a></li>
-                <li class="list-inline-item"><a class="nav-link" href="#">新規登録</a></li>
-            </ul>
-
-    </header>
-
     <footer class="mt-5">
         <nav class="navbar navbar-dark bg-warning">
             <div class="navbar-brand text-dark" style="font-size:x-large;">探求学園Laravel専攻</div>
