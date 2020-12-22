6c51b12525c5024bf6f5498b8fe80fea54bdcf0d
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-22 18:40:19 +0900

    modify:seederファイル修正

--- database/seeds/SalesStatusesTableSeeder.php
+++ database/seeds/SalesStatusesTableSeeder.php
@@ -12,22 +12,20 @@ class SalesStatusesTableSeeder extends Seeder
     public function run()
     {
         DB::table('m_sales_statuses')->insert([
-            [
-                'id' => '1',
-                'sale_status_name' => '在庫あり',
-            ],
-            [
-                'id' => '2',
-                'sale_status_name' => '残り5個',
-            ],
-            [
-                'id' => '3',
-                'sale_status_name' => '在庫無し',
-            ],
-            [
-                'id' => '4',
-                'sale_status_name' => '入荷未定',
-            ],
-        ]);
+            'id' => '1',
+            'sale_status_name' => '在庫有',
+            ]);
+        DB::table('m_sales_statuses')->insert([
+            'id' => '2',
+            'sale_status_name' => '在庫無',
+            ]);
+        DB::table('m_sales_statuses')->insert([
+            'id' => '3',
+            'sale_status_name' => '入荷待ち',
+            ]);
+        DB::table('m_sales_statuses')->insert([
+            'id' => '4',
+            'sale_status_name' => '入荷未定',
+            ]);
     }
 }
--- database/seeds/UsersClassificationsTableSeeder.php
+++ database/seeds/UsersClassificationsTableSeeder.php
@@ -12,26 +12,24 @@ class UsersClassificationsTableSeeder extends Seeder
     public function run()
     {
         DB::table('m_users_classifications')->insert([
-            [
-                'id' => '1',
-                'user_classification_name' => 'ゲスト',
-            ],
-            [
-                'id' => '2',
-                'user_classification_name' => '一般',
-            ],
-            [
-                'id' => '3',
-                'user_classification_name' => 'ゲスト',
-            ],
-            [
-                'id' => '4',
-                'user_classification_name' => '一般',
-            ],
-            [
-                'id' => '5',
-                'user_classification_name' => 'ゲスト',
-            ],
-        ]);
+            'id' => '1',
+            'user_classification_name' => '出品者',
+            ]);
+        DB::table('m_users_classifications')->insert([
+            'id' => '2',
+            'user_classification_name' => '購入者',
+            ]);
+        DB::table('m_users_classifications')->insert([
+            'id' => '3',
+            'user_classification_name' => 'ゲスト',
+            ]);
+        DB::table('m_users_classifications')->insert([
+            'id' => '4',
+            'user_classification_name' => '管理者',
+            ]);
+        DB::table('m_users_classifications')->insert([
+            'id' => '5',
+            'user_classification_name' => '開発者',
+            ]);
     }
 }
