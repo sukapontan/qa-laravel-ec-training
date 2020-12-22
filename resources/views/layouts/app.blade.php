4a0ee1d38ed42b5c0f5dd3160592dcdb0f4f5c6d
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-22 14:19:50 +0900

    add:app.blade.phpを追加

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>laravel-ec</title>
+        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+            integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" 
+            crossorigin="anonymous"
+        >
+
+    </head>
+
+    <body>
+
+    <header class="navbar navbar-dark bg-warning">
+
+            <a class="navbar-brand text-dark" style="font-size:x-large;" href="">
+                探求学園Laravel専攻
+            </a>
+
+            <ul class="list-inline navbar-brand text-dark">
+                <li class="list-inline-item"><a class="nav-link" href="#">ログイン</a></li>
+                <li class="list-inline-item"><a class="nav-link" href="#">新規登録</a></li>
+            </ul>
+
+    </header>
+
+    <footer class="mt-5">
+        <nav class="navbar navbar-dark bg-warning">
+            <div class="navbar-brand text-dark" style="font-size:x-large;">探求学園Laravel専攻</div>
+            <div class="navbar-brand text-dark" style="font-size: medium;">© 2020 QuestAcademia, All rights reserved.</div>
+        </nav>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+        crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+        crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+        crossorigin="anonymous">
+    </script>
+    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+
+</body>
+</html>
+
