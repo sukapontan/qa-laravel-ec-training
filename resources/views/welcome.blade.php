290c8c42dfe5ecd30a8946962f89bf534a1b4626
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-22 19:19:26 +0900

    add:top画面の文章を追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- <div class="center jumbotron bg-warning"></div> --}}
+    <div class="container">
+        <h1 class="text-center py-5">探求学園Laravel専攻 ECサイト</h1>
+        <div class="row">
+            <div class="col-xs-12 col-md-6 text-center mt-5">
+                <h5>まだアカウントを<br>お持ちでない方はこちら<h5>
+            </div>
+            <div class="col-xs-12 col-md-6 text-center mt-5">
+                <h5>すでにアカウントを<br>お持ちの方はこちら<h5>
+            </div>
+            <div class="w-100"></div>
+            <div class="col-xs-12 col-md-6 text-center pb-5">
+                <button type="button" class="btn btn-primary btn-md">新規登録</button>
+            </div>
+            <div class="colcol-xs-12 col-md-6 text-center pb-5">
+                <button type="button" class="btn btn-primary btn-md">ログイン</button>
+            </div>
+        </div>
+    </div>
 @endsection
