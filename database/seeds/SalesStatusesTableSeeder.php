feada65fae6f919eb853ab6944a472046b3265a5
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-22 10:42:48 +0900

    add:ダミーデータの追加

--- database/seeds/SalesStatusesTableSeeder.php
+++ database/seeds/SalesStatusesTableSeeder.php
@@ -12,9 +12,22 @@ class SalesStatusesTableSeeder extends Seeder
     public function run()
     {
         DB::table('m_sales_statuses')->insert([
-
-            'id' => '1',
-            'sale_status_name' => '在庫あり',
+            [
+                'id' => '1',
+                'sale_status_name' => '在庫あり',
+            ],
+            [
+                'id' => '2',
+                'sale_status_name' => '残り5個',
+            ],
+            [
+                'id' => '3',
+                'sale_status_name' => '在庫無し',
+            ],
+            [
+                'id' => '4',
+                'sale_status_name' => '入荷未定',
+            ],
         ]);
     }
 }
--- database/seeds/UsersClassificationsTableSeeder.php
+++ database/seeds/UsersClassificationsTableSeeder.php
@@ -12,9 +12,26 @@ class UsersClassificationsTableSeeder extends Seeder
     public function run()
     {
         DB::table('m_users_classifications')->insert([
-
-        'id' => '1',
-        'user_classification_name' => 'ゲスト',
+            [
+                'id' => '1',
+                'user_classification_name' => 'ゲスト',
+            ],
+            [
+                'id' => '2',
+                'user_classification_name' => '一般',
+            ],
+            [
+                'id' => '3',
+                'user_classification_name' => 'ゲスト',
+            ],
+            [
+                'id' => '4',
+                'user_classification_name' => '一般',
+            ],
+            [
+                'id' => '5',
+                'user_classification_name' => 'ゲスト',
+            ],
         ]);
     }
 }
