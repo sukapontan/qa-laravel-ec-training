664a1122fa46c97d5e71a316d7cab3305b2c07a6
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-23 12:01:58 +0900

    modify:HTMLのstyleタグを修正

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,8 +1,8 @@
 <header class="navbar navbar-dark bg-warning">
-    <a class="navbar-brand text-dark" style="font-size:x-large;" href="">
-        探求学園Laravel専攻
+    <a class="navbar-brand text-dark" href="">
+    探求学園Laravel専攻
     </a>
-    <ul class="list-inline navbar-brand text-dark">
+    <ul class="list-inline navbar-brand-01 text-dark">
         <li class="list-inline-item"><a class="nav-link" href="#">ログイン</a></li>
         <li class="list-inline-item"><a class="nav-link" href="#">新規登録</a></li>
     </ul>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
             http-equiv="X-UA-Compatible" content="ie=edge"
         >
         <title>laravel-ec</title>
-        <!-- Bootstrap -->
+        {{-- Bootstrap --}}
         <link rel="stylesheet" 
             href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
             integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" 
