1330c7031dc59c1143506134b112be70105be2e8
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-24 18:14:33 +0900

    modify:ログイン後のtop画面を修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,12 +2,30 @@
 
 @section('content')
 
-<div class="container">
-    <h1 class="text-center py-5">探求学園Laravel専攻　ECサイト<h1>
-    <h4 class="text-center py-5">
-        ご利用いただき、ありがとうございます<br>
-        右上のメニューから商品を検索したり、注文したりできます
-    </h4>
-</div>
+    <div class="text-center">
+        <h3 class="login_title text-left d-inline-block mt-5">ログイン</h3>
+    </div>
 
+    <div class="row mt-5 mb-5">
+        <div class="col-sm-6 offset-sm-3">
+
+            {!! Form::open(['route' => 'login.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('email', 'メールアドレス') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('ログイン', ['class' => 'btn btn btn-primary mt-2 offset-sm-5']) !!}
+            {!! Form::close() !!}
+
+            <p class="mt-3 offset-sm-3">{!! link_to_route('signup', 'まだ登録がお済みでない方はこちら') !!}</p>
+            
+        </div>
+    </div>
+    
 @endsection
