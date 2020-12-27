18c6a8b7307922fa8f499f94a3a456835ca7531c
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-27 18:07:08 +0900

    add:ログイン後の遷移先のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+//ログイン後
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/login_after', 'LoginAfterController@index')->name('login_after');
+});
