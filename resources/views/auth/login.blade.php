1c8b703b1fc54b92ea31d205ff1541dd10e6cf00
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-27 15:04:59 +0900

    modify:ログイン画面の修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,26 +5,20 @@
     <div class="text-center">
         <h3 class="login_title text-left d-inline-block mt-5">ログイン</h3>
     </div>
-
     <div class="row mt-5 mb-5">
         <div class="col-sm-6 offset-sm-3">
-
             {!! Form::open(['route' => 'login.post']) !!}
                 <div class="form-group">
                     {!! Form::label('email', 'メールアドレス') !!}
                     {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
-
                 <div class="form-group">
                     {!! Form::label('password', 'パスワード') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
-
                 {!! Form::submit('ログイン', ['class' => 'btn btn btn-primary mt-2 offset-sm-5']) !!}
             {!! Form::close() !!}
-
             <p class="mt-3 offset-sm-3">{!! link_to_route('signup', 'まだ登録がお済みでない方はこちら') !!}</p>
-            
         </div>
     </div>
     
