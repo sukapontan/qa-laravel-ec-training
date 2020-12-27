cc2de9113379e3c3db3e49139dcd29a0be79be52
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-27 18:13:15 +0900

    add:ログイン後表示ページの実装

---
+++ resources/views/login_after.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1 class="text-center py-5">探求学園Laravel専攻　ECサイト<h1>
+    <h4 class="text-center py-5">
+        ご利用いただき、ありがとうございます<br>
+        右上のメニューから商品を検索したり、注文したりできます
+    </h4>
+</div>
+    
+@endsection
