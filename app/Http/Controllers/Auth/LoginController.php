2cf16bda6246445daed369a0a4788b516e067c04
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-27 18:11:47 +0900

    add:ログイン後のリダイレクト先を追加

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/login_after';
 
     /**
      * Create a new controller instance.
