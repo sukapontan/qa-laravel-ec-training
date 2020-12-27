688cc0b41ad1b2c1406c5697c3cd03c94cfa6b06
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-27 18:18:26 +0900

    add:ログインしたユーザーの名前を表示させる

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -2,12 +2,9 @@
     <a class="navbar-brand text-dark" href="">
     探求学園Laravel専攻
     </a>
-    @if(Auth::check())
-            {{ Auth::user()->name }}
-    @endif
     <ul class="list-inline navbar-brand-01 text-dark">
         @if (Auth::check())
-        {{-- <li class="navbar bg-faded" style="flex-direction: column;">フクさん</li> --}}
+        <p class="text-center offset-9 pt-1">{{ Auth::user()->last_name}}さん</p>
         <li class="nav-item list-inline-item"><a href="" class="nav-link">商品検索</a></li>
         <li class="nav-item list-inline-item"><a href="" class="nav-link">カート</a></li>
         <li class="nav-item list-inline-item"><a href="" class="nav-link">注文履歴</a></li>
@@ -16,10 +13,10 @@
             {!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}
         </li>
         @else
-            <li class="nav-item list-inline-item">
-                {!! link_to_route('signup', '新規ユーザ登録', [], ['class' => 'nav-link']) !!}
-            </li>
             <li class="nav-item list-inline-item"><a href="#" class="nav-link">ログイン</a></li>
+            <li class="nav-item list-inline-item">
+                {!! link_to_route('signup', '新規登録', [], ['class' => 'nav-link']) !!}
+            </li>
         @endif
     </ul>
 </header>
