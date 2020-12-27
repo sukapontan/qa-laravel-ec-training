70eca93d22047657df15c34719ad55523baae252
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-27 18:45:12 +0900

    add:ボタンやタイトルに遷移先を追加

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,19 +1,19 @@
 <header class="navbar navbar-dark bg-warning">
-    <a class="navbar-brand text-dark" href="">
+    <a class="navbar-brand text-dark" href="/">
     探求学園Laravel専攻
     </a>
     <ul class="list-inline navbar-brand-01 text-dark">
         @if (Auth::check())
-        <p class="text-center offset-9 pt-1">{{ Auth::user()->last_name}}さん</p>
-        <li class="nav-item list-inline-item"><a href="" class="nav-link">商品検索</a></li>
-        <li class="nav-item list-inline-item"><a href="" class="nav-link">カート</a></li>
-        <li class="nav-item list-inline-item"><a href="" class="nav-link">注文履歴</a></li>
-        <li class="nav-item list-inline-item"><a href="" class="nav-link">ユーザー情報</a></li>
-        <li class="nav-item list-inline-item">
-            {!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}
-        </li>
+            <p class="text-center offset-9 pt-1">{{ Auth::user()->last_name}}さん</p>
+            <li class="nav-item list-inline-item"><a href="" class="nav-link">商品検索</a></li>
+            <li class="nav-item list-inline-item"><a href="" class="nav-link">カート</a></li>
+            <li class="nav-item list-inline-item"><a href="" class="nav-link">注文履歴</a></li>
+            <li class="nav-item list-inline-item"><a href="" class="nav-link">ユーザー情報</a></li>
+            <li class="nav-item list-inline-item">
+                {!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}
+            </li>
         @else
-            <li class="nav-item list-inline-item"><a href="#" class="nav-link">ログイン</a></li>
+            <li class="nav-item list-inline-item"><a href="login" class="nav-link">ログイン</a></li>
             <li class="nav-item list-inline-item">
                 {!! link_to_route('signup', '新規登録', [], ['class' => 'nav-link']) !!}
             </li>
