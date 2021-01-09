b306bc50f52366412ef4c808e75047e133dddde6
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2021-01-09 20:40:48 +0900

    add:商品情報画面のviewを追加

---
+++ resources/views/product/product_details_error.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-lg-12 text-center">
+            <h1>該当商品が見つかりませんでした...</h1>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-lg-12 text-center">
+            <h3>商品検索画面に戻り、やり直してください</h3>
+        </div>
+    </div>
+</div>
+<button type="submit" class="btn btn-primary offset-md-5 mt-2">商品検索画面へ</button>
+
+@endsection
