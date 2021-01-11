3429687b3eb6d20db7302b04960057cef99a261a
masayuki <masayuki201_201@yahoo.co.jp>
2021-01-11 14:17:23 +0900

    商品テーブル作成

---
+++ database/migrations/2021_01_11_092030_create_m_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_products', function (Blueprint $table) {
+            $table->increments('id')->comment('商品id');
+            $table->string('product_name', 64)->comment('商品名');
+            $table->integer('category_id')->comment('カテゴリー');
+            $table->integer('price')->unsigned()->comment('販売単価');
+            $table->string('description', 256)->nullable()->comment('商品説明');
+            $table->integer('sale_status_id')->comment('販売状態');
+            $table->integer('product_status_id')->comment('商品状態');
+            $table->timestamps('regist_date')->comment('商品登録');
+            $table->integer('user_id')->unsigned()->comment('ユーザid');
+            $table->char('delete_flag', 1)->comment('deleteフラグ');
+            //まだファイルがないのでコメント表示にしています
+            //$table->foreign('category_id')->references('id')->on('m_categories')->onDelete('cascade');
+
+            //$table->foreign('sale_status_id')->references('id')->on('m_sale_statuses)->onDelete('cascade');
+
+            //$table->foreign('product_status_id')->references('id')->on('m_product_statuses')->onDelete('cascade');
+
+            //$table->foreign('user_id')->references('id')->on('m_users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_products');
+    }
+}
