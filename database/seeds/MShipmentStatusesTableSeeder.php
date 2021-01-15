8843074c57f543f6ef1535ef770313d0cd7dd39f
paleo_engineer <g843522r@gmail.com>
2021-01-15 22:56:47 +0900

    m_shipment_statusesテーブルのSeeder内のテーブル名の記述を修正

--- database/seeds/MShipmentStatusesTableSeeder.php
+++ database/seeds/MShipmentStatusesTableSeeder.php
@@ -13,17 +13,17 @@ class MShipmentStatusesTableSeeder extends Seeder
     public function run()
     {
         // ダミー発送状態データのセット
-        DB::table('m_shipment_Statuses')->insert([
+        DB::table('m_shipment_statuses')->insert([
             'id' => '1',
             'shipment_status_name' => '発送準備中',
         ]);
 
-        DB::table('m_shipment_Statuses')->insert([
+        DB::table('m_shipment_statuses')->insert([
             'id' => '2',
             'shipment_status_name' => '発送済み',
         ]);
 
-        DB::table('m_shipment_Statuses')->insert([
+        DB::table('m_shipment_statuses')->insert([
             'id' => '3',
             'shipment_status_name' => '配送中',
         ]);
