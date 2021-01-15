52de2027bd3fa2467e176d6f3991e5d14dfe6aad
paleo_engineer <g843522r@gmail.com>
2021-01-15 23:04:21 +0900

    t_ordersテーブルのMigrationとSeederの記述を微修正

--- database/migrations/2021_01_11_020100_create_t_orders_table.php
+++ database/migrations/2021_01_11_020100_create_t_orders_table.php
@@ -17,7 +17,7 @@ class CreateTOrdersTable extends Migration
         Schema::create('t_orders', function (Blueprint $table) {
             $table->increments('id')->comment('注文ID'); // PK
             $table->integer('user_id')->unsigned()->comment('ユーザID'); // FK
-            $table->timestamp('ordar_date'); // 注文日
+            $table->timestamp('order_date'); // 注文日
 
             // ユーザIDの外部キー制約
             $table->foreign('user_id')->references('id')->on('m_users')->onDelete('cascade');
--- database/seeds/TOrdersTableSeeder.php
+++ database/seeds/TOrdersTableSeeder.php
@@ -15,16 +15,19 @@ class TOrdersTableSeeder extends Seeder
         // '注文'ダミーデータのセット
         DB::table('t_orders')->insert([
             'id' => '1',
+            'user_id' => '1',
             'order_date' => date('Y-m-d H:i:s'),
         ]);
 
         DB::table('t_orders')->insert([
             'id' => '2',
+            'user_id' => '2',
             'order_date' => date('Y-m-d H:i:s'),
         ]);
 
         DB::table('t_orders')->insert([
             'id' => '3',
+            'user_id' => '3',
             'order_date' => date('Y-m-d H:i:s'),
         ]);
     }
