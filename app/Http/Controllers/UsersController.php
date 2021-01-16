b030f7ed87008615d47e37df20760dfe0a60564b
daichi1123 <badass.haruzion@gmail.com>
2021-01-16 05:47:47 +0000

    add:ユーザ情報照会

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+
+class UsersController extends Controller
+{
+
+    public function index()
+    {   
+        return view('welcome');
+    }
+
+    // ユーザ情報の確認・修正・削除
+    public function show()
+    {
+        return view('users.users', ['user' => Auth::user() ]);
+    }
+
+    // public function edit()
+    // {
+    //     return view('user.edit', ['auth' => $auth ]);
+    // }
+
+    // public function update(Request $request, $id)
+    // {
+    //     $user_update = $request->all();
+    //     $user = Auth::user();
+    //     unset($form['_token']);
+    //     $user->fill($user_update)->save();
+
+    //     return redirect('/user_edit');
+    // }
+    
+    // public function destroy($id)
+    // {
+    //     $user = User::findOrFail($id);
+    //     $user->delete();
+
+    //     return view('/');
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'UsersController@index');
 
 // 新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
@@ -27,4 +25,8 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 //ログイン後
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/top', 'TopController@index')->name('top');
+    Route::get('users/users', 'UsersController@show')->name('users.show');
+    // Route::get('users/users/{id}/edit', 'UsersController@edit')->name('user.edit');
+    // Route::post('users/users/{id}/edit', 'UsersController@update')->name('user.update');
+    // Route::destroy('/', 'UsersController@destroy')->name('user.delete');
 });
