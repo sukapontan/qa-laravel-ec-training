e7fdb201f9188409d8173eba8fa1e1d3e37a3434
masayuki <masayuki201_201@yahoo.co.jp>
2021-01-19 04:14:33 +0900

    Top画面完成

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -7,15 +7,15 @@
     <div class="row">
         <div class="col-xs-12 col-md-6 text-center mt-5">
             <h5>まだアカウントを<br>お持ちでない方はこちら</h5>
-        </div>
-        <div class="col-xs-12 col-md-6 text-center pb-5">
-            <button type="button" class="btn btn-primary btn-md">新規登録</button>
+            <div class="col-xs-12 col-md-12 text-center mt-4">
+                <button type="button" class="btn btn-primary btn-md">新規登録</button>
+            </div>
         </div>
         <div class="col-xs-12 col-md-6 text-center mt-5">
             <h5>すでにアカウントを<br>お持ちの方はこちら</h5>
-        </div>
-        <div class="colcol-xs-12 col-md-6 text-center pb-5">
-            <button type="button" class="btn btn-primary btn-md">ログイン</button>
+            <div class="col-xs-12 col-md-12 text-center mt-4">
+                <button type="button" class="btn btn-primary btn-md">ログイン</button>
+            </div>
         </div>
     </div>
 </div>
