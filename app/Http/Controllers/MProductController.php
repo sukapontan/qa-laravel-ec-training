a59d328715ccc32ad1dc337fdf3c93cc3c39c23e
paleo_engineer <g843522r@gmail.com>
2021-01-20 08:54:08 +0900

    検索処理について実装用のコメントのみ追加

--- app/Http/Controllers/MProductController.php
+++ app/Http/Controllers/MProductController.php
@@ -15,8 +15,20 @@ class MProductController extends Controller
     public function index(Request $request)
     {
         // 商品一覧ページを表示する
+        // ModelからEloquentで全件取得する
         // ※現状はm_productテーブルがDBに無いのでエラーになる
         $items = MProduct::all();
+
+        // 検索フォームより受け取った文字列を$serch_textに格納
+        $serch_text = $request->serch_text;
+
+        // ここからロジックを実装する
+        // 具体的には分岐処理で、カテゴリidと商品名部分一致で検索を行う
+        // もしカテゴリが未選択で検索フォームも空白だったら処理は行わずにviewを表示する
+        // 入力があれば第1ソート=カテゴリ(昇順)、第2ソート=商品名(昇順)
+
+        // viewを表示する
+        // viewに渡すデータは何？商品と件数？件数は$query_numbersとか？
         return view('products.index', ['items' => $items]);
         
         // 作業メモ：$itemsはCollectionクラスのインスタンスである
