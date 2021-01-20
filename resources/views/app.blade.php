55db0161dbf33d8a8037d7a7ecc1adea2927aa63
paleo_engineer <g843522r@gmail.com>
2021-01-20 12:08:51 +0900

    bootstrapCSSの読み込みを追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- bootstrap CSSの読み込み --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <title>@yield('title')</title>
 </head>
