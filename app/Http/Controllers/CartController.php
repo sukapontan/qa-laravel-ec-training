e225f2e70069aa0f2d2b80ed07b3c6c30cb63c07
paleo_engineer <g843522r@gmail.com>
2021-02-01 09:17:00 +0900

    CartControllerを仮作成

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    // 商品データをカートに追加する(セッションを登録する)
+    // ※あとで商品詳細のViewからデータを受け取れるようにすること
+    public function addCart(Request $request)
+    {
+        $session_data = [
+            $request->product_id,
+            $request->quantity,
+        ];
+
+        $request->session()->put('session_data', $session_data);
+
+        // ※あとでカート内商品一覧へのリダイレクトにしておくこと
+        return redirect('/products');
+    }
+
+}
