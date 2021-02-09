26a93a4bd18d6c870b805242cf5b5160d056d913
masayuki <masayuki201_201@yahoo.co.jp>
2021-02-10 07:40:59 +0900

    update:edit.blade.php表示完了

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -4,79 +4,54 @@
 
 @section('content')
 <div class="container">
-    <h1 class="text-center py-5">ユーザー情報修正</h1>
-    {!! Form::open(['route' => 'signup.post']) !!}
-    <form>
-        <div class="form-group row">
+	<h1 class="text-center py-5">ユーザー情報修正</h1>
+    <form action="{{ route('user.update', [Auth::user()->id]) }}" method="put">
+    @csrf
+    <input type="hidden" name="id" value="{{Auth::user()->id}}">
+    <div class="form-group row">
         <label class="col-sm-2 text-sm-center offset-sm-1">氏名</label>
-
-        {!! Form::label('last_name', '姓') !!}
-        {!! Form::text('last_name', old('last_name'), ['class' => 'col-sm-1 text-sm-right col-2 col-form-label']) !!}
-        <div class="form-group col-sm-6 offset-sm-1">
-            {!! Form::label('first_name', '名') !!}
-            {!! Form::text('first_name', old('first_name'), ['class' => 'form-control col-sm-9']) !!}
-        </div>
-
-
         <label class="col-sm-1 text-sm-right col-2 col-form-label">姓</label>
-        <div class="col-sm-2">
-            <input type="text" class="form-control" value="" placeholder="〇〇">
-        </div>
+        <input type="text" name="last_name" value="{{ Auth::user()->last_name }}" class="col-sm-2">
         <label class="col-sm-1 text-sm-right col-2 col-form-label">名</label>
-        <div class="col-sm-2">
-          <input type="text" class="form-control" value="" placeholder="XX">
-        </div>
-      </div>
-
-      <div class="form-group row">
+        <input type="text" name="first_name" value="{{ Auth::user()->first_name }}" class="col-sm-2">
+    </div>
+    <div class="form-group row">
         <label class="col-sm-2 text-sm-center col-12 col-form-label offset-sm-1">住所</label>
         <label class="col-sm-1 text-right col-2 col-form-label">〒</label>
-        <div class="col-sm-3 col-8">
-          <input type="text" class="form-control" value="" placeholder="〇〇〇〇〇〇">
-        </div>
-      </div>
-      <div class="form-group row">
+        <input type="text" name="zipcode" value="{{ Auth::user()->zipcode }}" class="col-sm-3 col-8">
+    </div>
+    <div class="form-group row">
         <label class="col-sm-4 col-form-label text-sm-right">都道府県</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" value="" placeholder="富山県">
-        </div>
-      </div>
-      <div class="form-group row">
+        <input type="text" name="prefecture" value="{{ Auth::user()->prefecture }}" class="col-sm-3 col-8">
+    </div>
+    <div class="form-group row">
         <label class="col-sm-4 col-form-label text-sm-right">市町村区</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" value="" placeholder="南〇市">
-        </div>
-      </div>
-      <div class="form-group row">
+        <input type="text" name="municipality" value="{{ Auth::user()->municipality }}" class="col-sm-3 col-8">
+    </div>
+    <div class="form-group row">
         <label class="col-sm-4 col-form-label text-sm-right">番地</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" value="" placeholder="〇〇　〇-〇-〇">
-        </div>
-      </div>
-      <div class="form-group row">
+        <input type="text" name="address" value="{{ Auth::user()->address }}" class="col-sm-3 col-8">
+    </div>
+    <div class="form-group row">
         <label class="col-sm-4 col-form-label text-sm-right">マンション部屋番号</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" value=""　placeholder="あああ">
-        </div>
-      </div>
-      <div class="form-group row">
+        <input type="text" name="apartments" value="{{ Auth::user()->apartments }}" class="col-sm-3 col-8">
+    </div>
+    <div class="form-group row">
         <label class="col-sm-4 col-form-label text-sm-center">メールアドレス</label>
-        <div class="col-sm-6">
-          <input type="text" class="form-control" value="" placeholder="email@example.com">
-        </div>
-      </div>
-      <div class="form-group row">
+        <input type="text" name="email" value="{{ Auth::user()->email }}" class="col-sm-3 col-8">
+    </div>
+    <div class="form-group row">
         <label class="col-sm-4 col-form-label text-sm-center">電話番号</label>
-        <div class="col-sm-6">
-          <input type="text" class="form-control" value="" placeholder="〇〇〇〇〇〇〇〇〇〇">
-        </div>
-      </div>
-      <div class="d-flex justify-content-around py-5 col-sm-8 col-auto container">
-        <a href="" class="btn btn-primary px-1 col-md-2 col-2">修正</a>
-        <a href="" class="btn btn-danger px-1 col-md-2 col-2">退会</a>
-      </div>
+        <input type="text" name="phone_number" value="{{ Auth::user()->phone_number }}" class="col-sm-3 col-8">
+    </div>
+    <div class="d-flex justify-content-around py-5 col-sm-8 col-auto container">
+        <input class="btn btn-primary" type="submit" value="修正">
     </form>
-  </div>
-    {!! Form::close() !!}
 
+    <form action="{{ route('user.destroy', [Auth::user()->id]) }}" method="delete">
+    @csrf
+        <input class="btn btn-danger" type="submit" value="退会">
+    </form>
+    </div>
+</div>
 @endsection
