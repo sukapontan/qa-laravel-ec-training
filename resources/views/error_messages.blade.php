9c9b27d3db3e62c2cf7267d09774f7c474e87f5e
masayuki <masayuki201_201@yahoo.co.jp>
2021-02-14 14:02:23 +0900

    update:1次レビューご指摘箇所 error_messages.blade.php修正

--- resources/views/error_messages.blade.php
+++ resources/views/error_messages.blade.php
@@ -8,7 +8,7 @@
 
 {{-- フラッシュメッセージ --}}
 @if (session('flash_message'))
-<div class="flash_message alert alert-danger text-center py-3 my-0">
-    {{ session('flash_message') }}
-</div>
+    <div class="flash_message alert alert-danger text-center py-3 my-0">
+        {{ session('flash_message') }}
+    </div>
 @endif
