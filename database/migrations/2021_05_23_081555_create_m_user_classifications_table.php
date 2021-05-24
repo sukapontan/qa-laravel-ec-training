911c1332d6019fed3ea0e034d6d9a50f2caac588
Kou <hikkappi@yahoo.co.jp>
2021-05-24 20:20:37 +0900

    add: ユーザー周りのマイグレーションファイルを作成。

--- database/migrations/2021_05_23_081555_create_m_user_classifications_table.php
+++ database/migrations/2021_05_23_081555_create_m_user_classifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMUserClassificationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('m_user_classifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('user_classification_name', 32);
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('m_user_classifications');
     }
 }
