a240f11a10680378dfb5d47e5166c822ceee5a82
Kou <hikkappi@yahoo.co.jp>
2021-05-29 12:44:30 +0900

    fix: ヘッダー、フッターファイルを作成。

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-dark bg-warning">
+    <a class="navbar-brand text-dark" href="/" style="font-size:x-large;">全国特産品ECサイト</a>
+    <ul class="list-inline navbar-brand-01 text-dark">
+        <ul class="list-inline navbar-brand text-dark">
+            <li class="navbar bg-faded text-right" style="flex-direction: row-reverse;">Test</li>
+            <li class="list-inline-item"><a class="nav-link" href="#">商品検索</a></li>
+            <li class="list-inline-item"><a class="nav-link" href="#">カート</a></li>
+            <li class="list-inline-item"><a class="nav-link" href="#">注文履歴</a></li>
+            <li class="list-inline-item"><a class="nav-link" href="#">ユーザー情報</a></li>
+            <li class="list-inline-item"><a class="nav-link" href="#">ログアウト</a></li>
+        </ul>
+    </ul>
+</nav>
