e55aa375a6207c5aafb6e53defc1e77ebffc2b93
masaki420 <zyab5275@gmail.com>
2021-06-12 01:07:27 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::prefix('products')->group(function () {
     Route::get('/', 'ProductsController@index')->name('product.index');
