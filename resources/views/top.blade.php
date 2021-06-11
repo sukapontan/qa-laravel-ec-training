bc006bb737cae2329aea6111c312d677e7599b81
masaki420 <zyab5275@gmail.com>
2021-06-11 16:09:12 +0000

    test

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -14,7 +14,7 @@
 			<div class="col-xs-12 col-md-6 text-center mt-5">
 				<h5>すでにアカウントを<br>お持ちの方はこちら</h5>
 					<div class="col-xs-12 col-md-12 text-center mt-4">
-						<button type="button" class="btn btn-primary btn-md">ログイン</button>
+						<a href="login" class="btn btn-primary btn-md">ログイン</a>
 					</div>
 			</div>
 		</div>
