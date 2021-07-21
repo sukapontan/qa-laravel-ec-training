e82040bd3972fff97fcd877ef1cfc460d979d7e1
sukapontan <sukapontan.engineer@gmail.com>
2021-07-21 11:49:42 +0000

    軽微な修正

--- app/User.php
+++ app/User.php
@@ -23,10 +23,10 @@ class User extends Authenticatable
      * The attributes that should be hidden for arrays.
      *
      * @var array
-     */ $hidden = [
+     */protected $hidden = [
         'password', 'remember_token',
     ];
-
+    
     /**
      * The attributes that should be cast to native types.
      *
