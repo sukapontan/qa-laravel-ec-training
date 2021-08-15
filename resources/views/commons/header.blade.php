f8aef5a91dc753c0c093033dc19867338dbe1f72
sukapontan <sukapontan.engineer@gmail.com>
2021-08-15 07:27:27 +0000

    注文の詳細の作成

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -5,28 +5,28 @@
     <ul class="list-inline navbar-brand-01 text-dark">
         @if (Auth::check())
 
-            <p class="text-center offset-9 pt-1">{{-- {{ Auth::user()->last_name }}さん --}}</p>
-            <li class="nav-item list-inline-item">
-                <a class="nav-link" href="#">商品検索</a>
-            </li>
-            <li class="nav-item list-inline-item">
-                <a class="nav-link" href="#">カート</a>
-            </li>
-            <li class="nav-item list-inline-item">
-                <a class="nav-link" href="#">注文履歴</a>
-            </li>
-            <li class="nav-item list-inline-item">
-                <a class="nav-link" href="#">ユーザー情報</a>
-            </li>
-            <li class="nav-item list-inline-item">
-                <a class="nav-link" href="#">ログアウト</a>
-            </li>
+        <p class="text-center offset-9 pt-1">{{-- {{ Auth::user()->last_name }}さん --}}</p>
+        <li class="nav-item list-inline-item">
+            <a class="nav-link" href="{{ route('products.show') }}">商品検索</a>
+        </li>
+        <li class="nav-item list-inline-item">
+            <a class="nav-link" href="#">カート</a>
+        </li>
+        <li class="nav-item list-inline-item">
+            <a class="nav-link" href="#">注文履歴</a>
+        </li>
+        <li class="nav-item list-inline-item">
+            <a class="nav-link" href="#">ユーザー情報</a>
+        </li>
+        <li class="nav-item list-inline-item">
+            <a class="nav-link" href="#">ログアウト</a>
+        </li>
 
         @else
-            <ul class="list-inline navbar-brand text-dark">
-                <li class="list-inline-item"><a class="nav-link" href="{{ route('login') }}">ログイン</a></li>
-                <li class="list-inline-item"><a class="nav-link" href="#">新規登録</a></li>
-            </ul>
+        <ul class="list-inline navbar-brand text-dark">
+            <li class="list-inline-item"><a class="nav-link" href="{{ route('login') }}">ログイン</a></li>
+            <li class="list-inline-item"><a class="nav-link" href="#">新規登録</a></li>
+        </ul>
 
         @endif
     </ul>
